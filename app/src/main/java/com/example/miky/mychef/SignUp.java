0979a964e04e475c356dde84d4b15d5458aa972e
marcantella <marcantella@192.168.0.137>
2019-01-22 16:38:16 +0100

    Merge branch 'master' of https://github.com/alexcassi/MyChefAndroid
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/miky/mychef/SignUp.java
+++ app/src/main/java/com/example/miky/mychef/SignUp.java
@@ -1,7 +1,10 @@
 package com.example.miky.mychef;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends Activity {
 
@@ -9,5 +12,15 @@ public class SignUp extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button avviaSchermata = (Button)findViewById(R.id.button2);
+        avviaSchermata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent secondIntent = new Intent(SignUp.this,Login.class);
+                startActivity(secondIntent);
+            }
+        });
+
     }
 }
