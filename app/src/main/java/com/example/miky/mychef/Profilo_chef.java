893f88ddfad6f585a76114a111406878b63daf7c
MICHELA12 <m.biffi@itsrizzoli.it>
2019-03-04 14:50:16 +0100

    modificato logo nella schermata home

---
+++ app/src/main/java/com/example/miky/mychef/Profilo_chef.java
@@ -0,0 +1,13 @@
+package com.example.miky.mychef;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Profilo_chef extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profilo_chef);
+    }
+}
