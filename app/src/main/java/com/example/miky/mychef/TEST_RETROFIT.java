63c2fc93c27d868c9cdaae1e0913b6a69a8e13ff
marcantella <m.cantella@itsrizzoli.it>
2019-03-11 17:56:11 +0100

    test retrofit ed endpoint

--- app/src/main/java/com/example/miky/mychef/TEST_RETROFIT.java
+++ app/src/main/java/com/example/miky/mychef/TEST_RETROFIT.java
@@ -3,6 +3,8 @@ package com.example.miky.mychef;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -11,6 +13,7 @@ import java.util.List;
 
 import endpoint.MyApiEndpointInterface;
 import entità.Chef;
+import entità.Utente;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -32,8 +35,10 @@ public class TEST_RETROFIT extends Activity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        MyApiEndpointInterface apiService = retrofit.create(MyApiEndpointInterface.class);
+        final MyApiEndpointInterface apiService = retrofit.create(MyApiEndpointInterface.class);
+
         Call<List<Chef>> call = apiService.getChefs();
+
         call.enqueue(new Callback<List<Chef>>() {
             @Override
             public void onResponse(Call<List<Chef>> call, Response<List<Chef>> response) {
@@ -62,5 +67,42 @@ public class TEST_RETROFIT extends Activity {
             }
         });
 
+        Button login = findViewById(R.id.loginbutton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<Utente> call_utente = apiService.login("marco95.cl@hotmail.it", "123123");
+                call_utente.enqueue(new Callback<Utente>() {
+                    @Override
+                    public void onResponse(Call<Utente> call, Response<Utente> response) {
+                        int statusCode = response.code();
+                        Utente c = response.body();
+                        Log.w("la risposta", new Gson().toJson(response));
+                        Log.w("la risposta", response.toString());
+
+                        StringBuilder builder = new StringBuilder();
+                        builder.append(c.getEmail());
+                        builder.append(", ");
+                        builder.append(c.getPassword());
+                        builder.append(", ");
+                        builder.append(c.getNome());
+                        builder.append(", ");
+                        builder.append(c.getCognome());
+                        /*builder.append(", ");
+                        builder.append(c.getLuogo_lavoro());
+                        builder.append(", ");
+                        builder.append(c.getImmagine_profilo());
+                        builder.append("\n");*/
+
+                        tv.setText(builder.toString());
+                    }
+                        @Override
+                        public void onFailure(Call<Utente> call, Throwable t) {
+                            Log.w("errore", "errore");
+                        }
+                    });
+
+            }
+        });
     }
 }
