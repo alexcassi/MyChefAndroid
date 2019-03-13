b2bf64ae3174c15d4b89f55e535eeea05f29b867
MICHELA12 <m.biffi@itsrizzoli.it>
2019-03-13 17:35:53 +0100

    aggiunto pagina di modifica ricette

---
+++ app/src/main/java/com/example/miky/mychef/modifica_ricette.java
@@ -0,0 +1,13 @@
+package com.example.miky.mychef;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class modifica_ricette extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifica_ricette);
+    }
+}
--- app/src/main/java/com/example/miky/mychef/ricette.java
+++ app/src/main/java/com/example/miky/mychef/ricette.java
@@ -1,7 +1,10 @@
 package com.example.miky.mychef;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ricette extends Activity {
 
@@ -9,5 +12,14 @@ public class ricette extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ricette);
+
+        Button avviaSchermata = (Button)findViewById(R.id.edit);
+        avviaSchermata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent secondIntent = new Intent(ricette.this,modifica_ricette.class);
+                startActivity(secondIntent);
+            }
+        });
     }
 }
